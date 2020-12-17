11efb7b857a2c4bc7ad7368cef5d202808ef1411
lucynguyen <ngu.nguyen@kyanon.digital>
2020-12-17 15:23:36 +0700

    create a setActiveCategory function.

--- app/helpers.php
+++ app/helpers.php
@@ -2,3 +2,8 @@
 function presentPrice($price) {
   return '$'.number_format($price / 100, 2);
 }
+
+function setActiveCategory($category, $output = 'active')
+{
+    return request()->category == $category ? $output : '';
+}
